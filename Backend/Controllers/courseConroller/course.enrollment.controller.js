50adc6ea97eba90ad85ca329791ec86d27c2aef9
Aliimran1074 <aliimransachwani4@gmail.com>
2026-03-26 16:28:31 +0500

    Working on First Mode of Subscription

--- Backend/Controllers/courseConroller/course.enrollment.controller.js
+++ Backend/Controllers/courseConroller/course.enrollment.controller.js
@@ -1,17 +1,37 @@
 const courseModel = require("../../Models/CourseModels/course.model")
+const subscriptionModel = require("../../Models/SuperAdminModels/subscription.model")
+const studentRegistrationModel = require("../../Models/UserModels/studentRegistration.model")
 
 const courseEnrollment = async(req,res)=>{
     try{
-        const {instituteId,courseId,studentId}= req.body
-
+        const {courseId,studentId,subscriptionId}= req.body
+        const checkSubscriptionDetails = await subscriptionModel.findById(subscriptionId)
+        const instituteId = checkSubscriptionDetails.instituteId
+        const scopeOfInstitute= checkSubscriptionDetails.scopeType
         // sab se pehle check karna hai k jis course me enroll karna wo us institute ka hai bhi ya nhi or jo student register kar raha ho wo us institute ka hai ya nhi jis me yeh course parhaya jata hai 
 
         const courseDetails = await courseModel.findById(courseId)
         const courseInstituteId = courseDetails.instituteId
-        if(instituteId.toString == courseInstituteId.toString){
-            console.log("Course and Institute Match")
-            return res.status(200).json({message:"Mismatched"})
+        console.log(courseInstituteId)
+        if(!instituteId.toString() == courseInstituteId.toString()){
+            console.log("Course and Institute  Mismatch")
+            return res.status(200).json({message:" ID Mismatch"})
         }
+        // check student is of same institute or not
+        const checkStudentInstitute = await studentRegistrationModel.findById(studentId)
+        const instituteIdViaStudentId= checkStudentInstitute.instituteId
+
+        if(!instituteId.toString() == instituteIdViaStudentId.toString()){
+            console.log("Student and Institute  Mismatch")
+            return res.status(200).json({message:" ID Mismatch"})
+        }
+        if(scopeOfInstitute=='individual'){
+            // student can register in only one course
+        }
+
+        // check subscription also if student is of Basic Subscription he can only enroll in one Course
+        console.log("Mismatched")
+        return res.status(400).json({message:"Mismatched"})
     }
     catch(error){
         console.log("Error in Function",error)
--- Backend/Routers/courseRoutes.js
+++ Backend/Routers/courseRoutes.js
@@ -1,8 +1,11 @@
 const router= require('express').Router()
 const { courseCreation, updationInCourse, courseDeletion } = require('../Controllers/courseConroller/course.controller')
+const { courseEnrollment } = require('../Controllers/courseConroller/course.enrollment.controller')
 
 
 router.route('/courseCreation').post(courseCreation)
 router.route('/updateCourse/:id').put(updationInCourse)
 router.route('/courseDeletion').delete(courseDeletion)
+router.route('/courseEnrollment').post(courseEnrollment)
+
 module.exports=router
