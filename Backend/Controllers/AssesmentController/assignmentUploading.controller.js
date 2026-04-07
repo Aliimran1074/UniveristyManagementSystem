035afa15f32d135ddababb8bd1ed07d11b77456e
Aliimran1074 <aliimransachwani4@gmail.com>
2026-04-07 05:27:35 +0500

    Working on Assignment Uploading Function

--- Backend/Controllers/AssesmentController/assignmentUploading.controller.js
+++ Backend/Controllers/AssesmentController/assignmentUploading.controller.js
@@ -1,14 +1,17 @@
 const assignmentModel = require("../../Models/Assignment/assignment.model")
+const assignmentUploadingModel = require("../../Models/Assignment/assignmentUploading.model")
 const { courseEnrollmentModel } = require("../../Models/CourseModels/courseEnrollment.model")
 const studentRegistrationModel = require("../../Models/UserModels/studentRegistration.model")
 
+
+// is function ko run karke test karna hai
 const assignmentUploading= async(req,res)=>{
     try {
         
-        // if(!req.file){
-        // console.log("File Not Found")
-        // return res.status(400).json({message:"File Not Found"})
-        // }
+        if(!req.file){
+        console.log("File Not Found")
+        return res.status(400).json({message:"File Not Found"})
+        }
         const {assignmentId,studentId}= req.body
         const getAssignmentDetails = await assignmentModel.findById(assignmentId)
         
@@ -25,40 +28,57 @@ const assignmentUploading= async(req,res)=>{
             return res.status(400).json({message:"Sorry Only Enroll Student Can Upload Assignment"})
         }
         
-        // ids match karke upload karna hai bs
-        
-        
-        // const instituteDetailsViaAssignment = await assignmentModel.findById(assignmentId)
-        // const instituteDetailsViaStudent = await studentRegistrationModel.findById(studentId)
-        
-        // const instituteIdViaAssignment = instituteDetailsViaAssignment.instituteId
-        // const instituteIdViaStudent= instituteDetailsViaStudent.instituteId
-        
+        // check student already upload assignment or not
+        const checkAlreadyAssignmentUploading = await assignmentUploadingModel.find({assigmnetId:assignmentId,studentId:studentId})
 
-        
-        // if((instituteIdViaAssignment.toString())!== (instituteIdViaStudent.toString())){
-        //     console.log("Only Student of Same Institute Can Upload Assignment")
-        // } 
+        if(checkAlreadyAssignmentUploading){
+            console.log("Student Already Uplaod Assignment")
+            return res.status(400).json({message:"Student Already Uploaded Assignment"})
+        }
 
-        // console.log("Institute Details Via Assignment",instituteDetailsViaAssignment)
-        // console.log("Institute Details Via Student",instituteDetailsViaStudent)
+        const uploadAssignment = await assignmentUploadingModel.create({assigmnetId:assignmentId,studentId:studentId})
 
-        return res.status(200).json({message:"Thank you"})
-        // const fileName = `${courseName}_${Date.now()}`
+        if(!uploadAssignment){
+            console.log("Issue in Assignment Uploading")
+            return res.status(400).json({message:"Issue in Assignment Uploading"})
+        }
+         const fileName = `${studentId}_${Date.now()}`
         
-        //             const imageKitResponse= await imageKitConfig.upload({
-        //                 file:req.file.buffer,
-        //                 fileName:fileName
-        //             })
-        //             // console.log("Image kit response",imageKitResponse)
-        //             const imageKitUrl= imageKitResponse.url
-        //             console.log('Image kit url ', imageKitResponse.url)
-        //             if(imageKitUrl.length>0 || imageKitUrl){
-        //                 createManualAssignment.assignmentFile=imageKitUrl
-        //                 await createManualAssignment.save()
-        //             }
+                    const imageKitResponse= await imageKitConfig.upload({
+                        file:req.file.buffer,
+                        fileName:fileName
+                    })
+                    // console.log("Image kit response",imageKitResponse)
+                    const imageKitUrl= imageKitResponse.url
+                    console.log('Image kit url ', imageKitResponse.url)
+                    if(imageKitUrl.length>0 || imageKitUrl){
+                        uploadAssignment.uploadedFile=imageKitUrl
+                        await uploadAssignment.save()
+                    }
        
-        // return res.status(200).json({message:"Assignment Created Successfully",createManualAssignment})
+        return res.status(200).json({message:"Assignment Uploaded Successfully",uploadAssignment})
+
+
+        // // ids match karke upload karna hai bs
+        
+        
+        // // const instituteDetailsViaAssignment = await assignmentModel.findById(assignmentId)
+        // // const instituteDetailsViaStudent = await studentRegistrationModel.findById(studentId)
+        
+        // // const instituteIdViaAssignment = instituteDetailsViaAssignment.instituteId
+        // // const instituteIdViaStudent= instituteDetailsViaStudent.instituteId
+        
+
+        
+        // // if((instituteIdViaAssignment.toString())!== (instituteIdViaStudent.toString())){
+        // //     console.log("Only Student of Same Institute Can Upload Assignment")
+        // // } 
+
+        // // console.log("Institute Details Via Assignment",instituteDetailsViaAssignment)
+        // // console.log("Institute Details Via Student",instituteDetailsViaStudent)
+
+        // return res.status(200).json({message:"Thank you"})
+       
 
     } catch (error) {
         console.log("Error in Assignment Uploading Function",error)
