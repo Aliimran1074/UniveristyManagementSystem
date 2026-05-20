4bdc3c1b1b4ad0801da66b21bfc8c641935135bc
Aliimran1074 <aliimransachwani4@gmail.com>
2026-05-20 16:08:38 +0500

    Working on Integration of Checker Agent

--- Backend/Controllers/AssesmentController/assignementCheckerUsingAI.js
+++ Backend/Controllers/AssesmentController/assignementCheckerUsingAI.js
@@ -1,8 +1,24 @@
+const { default: axios } = require("axios")
+const assignmentModel = require("../../Models/Assignment/assignment.model")
+const assignmentUploadingModel = require("../../Models/Assignment/assignmentUploading.model")
+
 const assignmentCheckerFunction = async(req,res)=>{
     try{
 
         // questions as string bhejne hai or assignment upload file bhejni hai Ai ko
         const {uploadAssignmentId} = req.body
+        const isUploadAssignmentExist = await assignmentUploadingModel.findById(uploadAssignmentId)
+        if(!isUploadAssignmentExist){
+            console.log("Assignment Uploading not Exist ")
+            return res.status(400).json({message:"Assignment Uploading not Exist"})
+        }
+
+        const assigmentId = isUploadAssignmentExist.assigmnetId
+        const getAssignmentQuestions = await assignmentModel.findById(assigmentId).select(assignmentQuestions)
+
+        const getUploadFileLink = isUploadAssignmentExist.uploadedFile
+
+        // const getResponseFromAi = await axios.post()
     }
     catch(error){
 
