070b7fa970feee74b5b243a123c0c51a72de97fa
Aliimran1074 <aliimransachwani4@gmail.com>
2026-05-20 21:13:48 +0500

    Working on checker agent

--- Backend/Controllers/AssesmentController/assignementCheckerUsingAI.js
+++ Backend/Controllers/AssesmentController/assignementCheckerUsingAI.js
@@ -1,7 +1,8 @@
 const { default: axios } = require("axios")
 const assignmentModel = require("../../Models/Assignment/assignment.model")
 const assignmentUploadingModel = require("../../Models/Assignment/assignmentUploading.model")
-
+const FormData= require('form-data')
+const studentModel = require("../../Models/UserModels/students.models")
 const assignmentCheckerFunction = async(req,res)=>{
     try{
 
@@ -14,11 +15,26 @@ const assignmentCheckerFunction = async(req,res)=>{
         }
 
         const assigmentId = isUploadAssignmentExist.assigmnetId
+        const studentId = isUploadAssignmentExist.studentId
+        const getStudentName = await studentModel.findById(studentId).select(name)
         const getAssignmentQuestions = await assignmentModel.findById(assigmentId).select(assignmentQuestions)
 
         const getUploadFileLink = isUploadAssignmentExist.uploadedFile
+        const formData =new FormData()
+        const fileResponse = await axios.get(getUploadFileLink,{responseType:'arraybuffer'})
 
-        // const getResponseFromAi = await axios.post()
+        const pdfBuffer = Buffer.from(fileResponse.data)
+
+        // one student can upload more than one assignment according to this logic student uploading file name always same and cause overwrite
+        formData.append('file',pdfBuffer,{
+            filename:`${getStudentName} assignment File ${Date.now()}`,
+            contentType:'application/pdf'
+        })
+
+        formData.append('questions',JSON.stringify(getAssignmentQuestions))
+
+        const getResponseFromAi = await axios.post()
+        // https://huggingface-configuration.vercel.app/assignment/assignmentChecker
     }
     catch(error){
 
